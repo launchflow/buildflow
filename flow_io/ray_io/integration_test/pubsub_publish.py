52ea2f6003df80b5208a463048f48d14d5144932
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-02-02 13:58:01 -0600

    remove options

--- flow_io/ray_io/integration_test/pubsub_publish.py
+++ flow_io/ray_io/integration_test/pubsub_publish.py
@@ -2,7 +2,7 @@ import json
 
 from google.cloud import pubsub_v1
 
-topic = 'projects/pubsub-test-project/topics/incoming_topic'
+# topic = 'projects/pubsub-test-project/topics/incoming_topic'
 topic = 'projects/pubsub-test-project/topics/node_76e3'
 client = pubsub_v1.PublisherClient()
 print('Publishing to: ', topic)
