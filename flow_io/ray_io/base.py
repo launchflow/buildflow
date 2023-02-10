b70ac7f4031c83e6ef081ba8510d4fbc28bfa811
tanke <jtanke@uoregon.edu>
2023-02-09 16:05:38 -0800

    trace

--- flow_io/ray_io/base.py
+++ flow_io/ray_io/base.py
@@ -47,6 +47,8 @@ def set_header_in_ctx(ctx, key, value):
 def add_to_span(key: str, data: Union[Dict[str, Any], Iterable[Dict[str, Any]]], ctx: Dict[str, str]):
     print("ADDING TO SPAN: ", key, data, ctx)
     with tracer.start_as_current_span(key, context=ctx) as span:
+        print('Tracer: ', tracer)
+        print('span: ', span)
         span.set_attribute(key, json.dumps(data))
         print('testing')
 
