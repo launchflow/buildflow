e9accf0fbf0c705f49c874fe8d578792200aa1fb
tanke <jtanke@uoregon.edu>
2023-02-16 16:27:49 -0800

    fix imports

--- flow_io/ray_io/__init__.py
+++ flow_io/ray_io/__init__.py
@@ -2,25 +2,24 @@ from typing import Callable
 
 import ray
 
-import flow_io
-from flow_io import flowstate
+from flow_io import flow_state, resources
 from flow_io.ray_io import bigquery, pubsub
 
 # TODO: Add support for other IO types.
 _IO_TYPE_TO_SOURCE = {
-    flow_io.BigQuery.__name__: bigquery.BigQuerySourceActor,
-    flow_io.PubSub.__name__: pubsub.PubSubSourceActor,
+    resources.BigQuery.__name__: bigquery.BigQuerySourceActor,
+    resources.PubSub.__name__: pubsub.PubSubSourceActor,
 }
 
 # TODO: Add support for other IO types.
 _IO_TYPE_TO_SINK = {
-    flow_io.BigQuery.__name__: bigquery.BigQuerySinkActor,
-    flow_io.PubSub.__name__: pubsub.PubsubSinkActor,
+    resources.BigQuery.__name__: bigquery.BigQuerySinkActor,
+    resources.PubSub.__name__: pubsub.PubsubSinkActor,
 }
 
 
 def run(remote_fn: Callable):
-    node_state = flowstate.get_node_state(flowstate.get_node_launch_file())
+    node_state = flow_state.get_node_state(flow_state.get_node_launch_file())
     sink_actors = [
         _IO_TYPE_TO_SINK[output_ref.__class__.__name__].remote(
             remote_fn, output_ref) for output_ref in node_state.output_refs
