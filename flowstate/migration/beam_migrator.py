e3ccd9e723086b3d74494c4df35fcc4306ec6222
tanke <jtanke@uoregon.edu>
2023-02-19 20:16:19 -0800

    update comment

--- flowstate/migration/beam_migrator.py
+++ flowstate/migration/beam_migrator.py
@@ -1,3 +1,3 @@
 # This module will attempt to automatically migrate an Apache Beam pipeline to
-# a Ray pipeline. It is not a perfect translation, but it should be a good
+# a FlowState pipeline. It is not a perfect translation, but it should be a good
 # starting point for most pipelines.
