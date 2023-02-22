2e6e31348ce8bb9dcee95d95a3921d1d82d34e75
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-02-22 10:38:24 -0600

    remove duckdb from import since it is not fully implemented

--- buildflow/api/__init__.py
+++ buildflow/api/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .processor import ProcessorAPI
-from .resources import IO, BigQuery, DuckDB, Empty, PubSub, RedisStream
+from .resources import IO, BigQuery, Empty, PubSub, RedisStream
 from .schema import Schema
 
 # NOTE: Only API code should go into this directory. Any runtime code should go
