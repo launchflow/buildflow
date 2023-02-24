ba6312e2a480aef4220ea70714167c8096239fc0
tanke <jtanke@uoregon.edu>
2023-02-24 00:18:32 -0800

    update example

--- samples/class_sample.py
+++ samples/class_sample.py
@@ -6,11 +6,13 @@ steps to run:
     3. python class_sample.py
 """
 
+from typing import Any, Dict
+
 import buildflow as flow
-import time
 
 # TODO(developer): Fill in with a pub/sub subscription.
-_SUBSCRIPTION = 'projects/daring-runway-374503/subscriptions/taxi-data'
+# 'projects/{PROJECT_ID}/subscriptions/{SUBSCRIPTION_NAME}'
+_SUBSCRIPTION = ''
 
 
 class MyProcessor(flow.Processor):
@@ -20,11 +22,11 @@ class MyProcessor(flow.Processor):
         return flow.PubSub(subscription=_SUBSCRIPTION)
 
     def _setup(self):
-        # this is where you would initialize any clients / shared state
-        pass
+        self.client = ...
 
-    def process(self, taxi_info):
-        return taxi_info
+    def process(self, message_data: Dict[str, Any]):
+        print(message_data)
+        return message_data
 
 
 flow.run(MyProcessor)
