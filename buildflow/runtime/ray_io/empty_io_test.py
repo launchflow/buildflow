583d4994e51c2f5c57a7fadc1ac182b1fdef1089
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-03-03 10:14:38 -0600

    fix: add ability to run more than one flow at a time

--- buildflow/runtime/ray_io/empty_io_test.py
+++ buildflow/runtime/ray_io/empty_io_test.py
@@ -22,7 +22,7 @@ class EmptyTest(unittest.TestCase):
         output = self.flow.run()
 
         self.assertEqual(len(output), 1)
-        self.assertEqual(list(output.values())[0], [1, 2, 3])
+        self.assertEqual(output, {'process': {'local': [1, 2, 3]}})
 
     def test_end_to_end_empty_multi_output(self):
 
@@ -33,7 +33,26 @@ class EmptyTest(unittest.TestCase):
         output = self.flow.run()
 
         self.assertEqual(len(output), 1)
-        self.assertEqual(list(output.values())[0], [[1, 1], [2, 2], [3, 3]])
+        self.assertEqual(output,
+                         {'process': {
+                             'local': [[1, 1], [2, 2], [3, 3]]
+                         }})
+
+    def test_end_to_end_with_class(self):
+
+        class MyProcessor(buildflow.Processor):
+
+            @staticmethod
+            def _input():
+                return buildflow.Empty([1, 2, 3])
+
+            def process(self, num: int):
+                return num
+
+        output = self.flow.run(MyProcessor)
+
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output, {'MyProcessor': {'local': [1, 2, 3]}})
 
 
 if __name__ == '__main__':
