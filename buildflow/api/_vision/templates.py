679e3ff41fb10950384f66b9279a24dba3643c06
tanke <jtanke@uoregon.edu>
2023-03-06 12:39:09 -0800

    cleanup io

--- buildflow/api/_vision/templates.py
+++ buildflow/api/_vision/templates.py
@@ -1,6 +1,4 @@
 import dataclasses
-from enum import Enum
-from typing import Any, Dict
 
 from buildflow.api.io import IO, HTTPEndpoint, PubSub
 from buildflow.api.processor import ProcessorAPI
@@ -8,22 +6,10 @@ from buildflow.api.processor import ProcessorAPI
 
 class Template:
     """Super class for all template types."""
-    _template_type: str
-
-    @classmethod
-    def from_config(cls, node_info: Dict[str, Any]):
-        template_type = node_info['_template_type']
-        return _TEMPLATE_MAPPING[template_type](**node_info)
 
     # This instance method defines the reference to the managed Processor.
     def processor(self) -> ProcessorAPI:
         raise NotImplementedError('processor not implemented')
-
-
-class TemplateType(Enum):
-    CloudRun = 'CLOUD_RUN'
-    GCSFileEventStream = 'GCS_FILE_EVENT_STREAM'
-    CloudScheduler = 'CLOUD_SCHEDULER'
 
 
 @dataclasses.dataclass(frozen=True)
@@ -32,8 +18,6 @@ class CloudRun(Template):
     public_access: bool
     endpoint: HTTPEndpoint
 
-    _template_type: str = TemplateType.CloudRun.value
-
 
 @dataclasses.dataclass(frozen=True)
 class GCSFileEventStream(Template):
@@ -41,20 +25,9 @@ class GCSFileEventStream(Template):
     pubsub: PubSub
     sink: IO
 
-    _template_type: str = TemplateType.GCSFileEventStream.value
-
 
 @dataclasses.dataclass(frozen=True)
 class CloudScheduler(Template):
     cron_schedule: str
     source: IO
     sink: IO
-
-    _template_type: str = TemplateType.CloudScheduler.value
-
-
-_TEMPLATE_MAPPING = {
-    TemplateType.CloudRun.value: CloudRun,
-    TemplateType.GCSFileEventStream.value: GCSFileEventStream,
-    TemplateType.CloudScheduler.value: CloudScheduler,
-}
--- buildflow/api/io.py
+++ buildflow/api/io.py
@@ -1,42 +1,25 @@
 import dataclasses
-from enum import Enum
 from typing import Any, Dict, List, TypeVar
 
 
 class InputOutput:
     """Super class for all input and output types."""
-    _io_type: str
-
-    @classmethod
-    def from_config(cls, node_info: Dict[str, Any]):
-        io_type = node_info['_io_type']
-        return _IO_MAPPING[io_type](**node_info)
+    pass
 
 
 IO = TypeVar('IO', bound=InputOutput)
-
-
-class IOType(Enum):
-    Pubsub = 'PUBSUB'
-    BigQuery = 'BIG_QUERY'
-    RedisStream = 'REDIS_STREAM'
-    DuckDB = 'DUCKDB'
-    Empty = 'EMPTY'
-    HTTPEndpoint = 'HTTP_ENDPOINT'
 
 
 @dataclasses.dataclass(frozen=True)
 class HTTPEndpoint(InputOutput):
     host: str = 'localhost'
     port: int = 3569
-    _io_type: str = IOType.HTTPEndpoint.value
 
 
 @dataclasses.dataclass(frozen=True)
 class PubSub(InputOutput):
     topic: str = ''
     subscription: str = ''
-    _io_type: str = IOType.Pubsub.value
 
 
 @dataclasses.dataclass(frozen=True)
@@ -57,9 +40,6 @@ class BigQuery(InputOutput):
     # will attempt to create one.
     temp_gcs_bucket: str = ''
 
-    batch_size: int = 1000
-    _io_type: str = IOType.BigQuery.value
-
 
 @dataclasses.dataclass(frozen=True)
 class RedisStream(InputOutput):
@@ -69,7 +49,6 @@ class RedisStream(InputOutput):
     start_positions: Dict[str, str] = dataclasses.field(default_factory=dict)
     # Read timeout. If > 0 this is how long we will read from the redis stream.
     read_timeout_secs: int = -1
-    _io_type: str = IOType.RedisStream.value
 
 
 @dataclasses.dataclass(frozen=True)
@@ -77,20 +56,8 @@ class DuckDB(InputOutput):
     database: str
     table: str = ''
     query: str = ''
-    _io_type: str = IOType.DuckDB.value
 
 
 @dataclasses.dataclass(frozen=True)
 class Empty(InputOutput):
     inputs: List[Any] = dataclasses.field(default_factory=list)
-    _io_type: str = IOType.Empty.value
-
-
-_IO_MAPPING = {
-    IOType.BigQuery.value: BigQuery,
-    IOType.Pubsub.value: PubSub,
-    IOType.RedisStream.value: RedisStream,
-    IOType.DuckDB.value: DuckDB,
-    IOType.Empty.value: Empty,
-    IOType.HTTPEndpoint.value: HTTPEndpoint,
-}
