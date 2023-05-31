3b6be9e3a182671aa84552c3621b1cf3a5e8d2d2
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-05-31 12:04:11 -0500

    add top level provider for GCS file stream

--- buildflow/io/registry.py
+++ buildflow/io/registry.py
@@ -1,34 +1,49 @@
-import dataclasses
+from dataclasses import dataclass
 
 from buildflow.io.providers.gcp.gcp_pub_sub import GCPPubSubProvider
 from buildflow.io.providers.gcp.bigquery import StreamingBigQueryProvider
+from buildflow.io.providers.gcp.gcs_file_stream import GCSFileStreamProvider
 
 
-@dataclasses.dataclass
+@dataclass
 class GCPPubSubSubscription:
     topic_id: str
     subscription_id: str
 
     def provider(self):
         # 'projects/daring-runway-374503/subscriptions/taxiride-sub')
-        billing_project_id = self.subscription_id.split('/')[1]
+        billing_project_id = self.subscription_id.split("/")[1]
         batch_size = 1000
-        return GCPPubSubProvider(billing_project_id=billing_project_id,
-                                 topic_id=self.topic_id,
-                                 subscription_id=self.subscription_id,
-                                 batch_size=batch_size)
+        return GCPPubSubProvider(
+            billing_project_id=billing_project_id,
+            topic_id=self.topic_id,
+            subscription_id=self.subscription_id,
+            batch_size=batch_size,
+        )
 
 
-@dataclasses.dataclass
+@dataclass
 class BigQueryTable:
     table_id: str
 
     def provider(self):
-        billing_project_id = self.table_id.split('.')[0]
-        return StreamingBigQueryProvider(billing_project_id=billing_project_id,
-                                         table_id=self.table_id)
+        billing_project_id = self.table_id.split(".")[0]
+        return StreamingBigQueryProvider(
+            billing_project_id=billing_project_id, table_id=self.table_id
+        )
 
 
-@dataclasses.dataclass
+@dataclass
+class GCSFileStream:
+    bucket_name: str
+    project_id: str
+
+    def provider(self):
+        return GCSFileStreamProvider(
+            bucket_name=self.bucket_name, project_id=self.project_id
+        )
+
+
+@dataclass
 class EmptySink:
     pass
