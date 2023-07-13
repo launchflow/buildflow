ebaeae8b7ad548a4f98055c71fc2e88c6b50fcb5
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-07-13 11:31:55 -0500

    make flow options require keywords

--- buildflow/core/options/flow_options.py
+++ buildflow/core/options/flow_options.py
@@ -1,46 +1,70 @@
-import dataclasses
-
 from buildflow.core.options._options import Options
 from buildflow.core.options.infra_options import InfraOptions, PulumiOptions
 from buildflow.core.options.runtime_options import AutoscalerOptions, RuntimeOptions
 
 
-@dataclasses.dataclass
 class FlowOptions(Options):
-    # Runtime options:
-    #   The number of replicas to start with
-    num_replicas: int = 1
-    #   Log level for runtime
-    runtime_log_level: str = "INFO"
-    #   Whether or not autoscaling shoule be enabled.
-    enable_autoscaler: bool = True
-    #   Minimum number of replicas to scale down to.
-    min_replicas: int = 1
-    #   Maximum number of replicas to scale up to.
-    max_replicas: int = 1000
-    #   The threshold for how long it will take to burn down a
-    #   backlog before scaleing up. Increasing this number
-    #   will cause your pipeline to scale up more aggresively.
-    pipeline_backlog_burn_threshold: int = 60
-    #   The target cpu percentage for scaling down. Increasing
-    #   this number will cause your pipeline to scale down more
-    #   aggresively.
-    pipeline_cpu_percent_target: int = 25
-    # Infra options
-    #   Whether schema validation should be enabled.
-    #   Valid values are: strict, warning, none
-    schema_validation: str = "none"
-    #   Whether or not confirmation should be required before applying changes.
-    require_confirmation: bool = True
-    #   Whether destroy projection should be enabled for pulumi.
-    enable_destroy_protection: bool = False
-    #   Whether or not to refresh state before applying changes.
-    refresh_state: bool = True
-    #   Log level for infra
-    infra_log_level: str = "INFO"
+    def __init__(
+        self,
+        *,
+        # Runtime options
+        num_replicas: int = 1,
+        runtime_log_level: str = "INFO",
+        # Autoscaler options
+        enable_autoscaler: bool = True,
+        min_replicas: int = 1,
+        max_replicas: int = 1000,
+        pipeline_backlog_burn_threshold: int = 60,
+        pipeline_cpu_percent_target: int = 25,
+        # Infra options
+        schema_validation: str = "none",
+        require_confirmation: bool = True,
+        infra_log_level: str = "INFO",
+        # Pulumi options
+        enable_destroy_protection: bool = False,
+        refresh_state: bool = True,
+    ) -> None:
+        """Options for configuring a Flow.
 
-    def __post_init__(self):
-        self._runtime_options = RuntimeOptions(
+        Args:
+            num_replicas (int): The number of replicas to start with. Defaults to 1.
+            runtime_log_level (str): The log level for the runtime. Defaults to "INFO".
+            enable_autoscaler (bool): Whether or not autoscaling should be enabled.
+                Defaults to True.
+            min_replicas (int): The minimum number of replicas to scale down to.
+                Defaults to 1.
+            max_replicas (int): The maximum number of replicas to scale up to.
+                Defaults to 1000.
+            pipeline_backlog_burn_threshold (int): The threshold for how long it will
+                take to burn down a backlog before scaling up. Increasing this number
+                will cause your pipeline to scale up more aggresively. Defaults to 60.
+            pipeline_cpu_percent_target (int): The target cpu percentage for scaling
+                down. Increasing this number will cause your pipeline to scale down
+                more aggresively. Defaults to 25.
+            schema_validation (str): Whether schema validation should be enabled.
+                Valid values are: strict, warning, none. Defaults to "none".
+            require_confirmation (bool): Whether or not confirmation should be
+                required before applying changes. Defaults to True.
+            infra_log_level (str): The log level for the infra. Defaults to "INFO".
+            enable_destroy_protection (bool): Whether destroy projection should be
+                enabled for pulumi. Defaults to False.
+            refresh_state (bool): Whether or not to refresh state before applying
+                changes. Defaults to True.
+        """
+        super().__init__()
+        self.num_replicas = num_replicas
+        self.runtime_log_level = runtime_log_level
+        self.enable_autoscaler = enable_autoscaler
+        self.min_replicas = min_replicas
+        self.max_replicas = max_replicas
+        self.pipeline_backlog_burn_threshold = pipeline_backlog_burn_threshold
+        self.pipeline_cpu_percent_target = pipeline_cpu_percent_target
+        self.schema_validation = schema_validation
+        self.require_confirmation = require_confirmation
+        self.infra_log_level = infra_log_level
+        self.enable_destroy_protection = enable_destroy_protection
+        self.refresh_state = refresh_state
+        self.runtime_options = RuntimeOptions(
             processor_options={},
             num_replicas=self.num_replicas,
             log_level=self.runtime_log_level,
@@ -53,7 +77,7 @@ class FlowOptions(Options):
                 pipeline_cpu_percent_target=self.pipeline_cpu_percent_target,
             ),
         )
-        self._infra_options = InfraOptions(
+        self.infra_options = InfraOptions(
             pulumi_options=PulumiOptions(
                 enable_destroy_protection=self.enable_destroy_protection,
                 refresh_state=self.refresh_state,
@@ -63,14 +87,6 @@ class FlowOptions(Options):
             require_confirmation=self.require_confirmation,
             log_level=self.infra_log_level,
         )
-
-    @property
-    def runtime_options(self) -> RuntimeOptions:
-        return self._runtime_options
-
-    @property
-    def infra_options(self) -> InfraOptions:
-        return self._infra_options
 
     @classmethod
     def default(cls) -> Options:
