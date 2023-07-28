2463ad5dc9d32bb38ed5bf8856361c4d17143aff
Caleb Van Dyke <ctvandyke24@gmail.com>
2023-07-28 09:05:52 -0500

    add missing file

--- buildflow/samples/csv_bigquery_walkthrough/main.py
+++ buildflow/samples/csv_bigquery_walkthrough/main.py
@@ -27,11 +27,11 @@ source = GCSFileChangeStream(
     ).options(managed=True, force_destroy=True),
     pubsub_topic=GCPPubSubTopic(
         project_id=gcp_project,
-        topic_name="storage_events_topic",
+        topic_name=f"{bucket_name}_storage_events_topic",
     ).options(managed=True),
     pubsub_subscription=GCPPubSubSubscription(
         project_id=gcp_project,
-        subscription_name="storage_events_sub",
+        subscription_name=f"{bucket_name}_storage_events_sub",
         topic_id=f"projects/{gcp_project}/topics/{bucket_name}_storage_events_topic",
     ).options(managed=True),
 )
