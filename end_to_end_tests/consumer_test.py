46619644b0a9a14f1b5efdde86c9848214a37970
CalebTVanDyke <ctvandyke24@gmail.com>
2023-10-19 13:40:33 -0500

    actually rever tests

--- end_to_end_tests/consumer_test.py
+++ end_to_end_tests/consumer_test.py
@@ -1,32 +1,24 @@
 import asyncio
-import dataclasses
 import os
+import shutil
 import tempfile
 import unittest
+from typing import Dict
 
+import duckdb
 import pytest
-import requests
 
 import buildflow
 from buildflow.dependencies.base import Scope, dependency
-from buildflow.dependencies.sink import SinkDependencyBuilder
-from buildflow.io.local.file import File
-from buildflow.types.portable import FileFormat
-
-
-@dataclasses.dataclass
-class InputRequest:
-    val: int
-
-
-@dataclasses.dataclass
-class OutputResponse:
-    val: int
+from buildflow.io.local import File
+from buildflow.io.portable.file_change_stream import FileChangeStream
+from buildflow.io.portable.table import AnalysisTable
+from buildflow.types.portable import FileChangeEvent, FileFormat
 
 
 @pytest.mark.usefixtures("ray")
 @pytest.mark.usefixtures("event_loop_instance")
-class EndpointLocalTest(unittest.TestCase):
+class FileStreamLocalTest(unittest.TestCase):
     def get_async_result(self, coro):
         """Run a coroutine synchronously."""
         return self.event_loop.run_until_complete(coro)
@@ -45,78 +37,130 @@ class EndpointLocalTest(unittest.TestCase):
 
         return self.event_loop.run_until_complete(wait_wrapper())
 
-    def test_endpoint_end_to_end(self):
-        app = buildflow.Flow()
-        service = app.service(num_cpus=0.5)
+    def setUp(self) -> None:
+        self.dir_to_watch = tempfile.mkdtemp()
+        self.table = "end_to_end_test"
+        self.database = os.path.join(os.getcwd(), "buildflow_managed.duckdb")
 
-        @service.endpoint(route="/test", method="POST")
-        def my_endpoint(input: InputRequest) -> OutputResponse:
-            return OutputResponse(input.val + 1)
+    def tearDown(self) -> None:
+        shutil.rmtree(self.dir_to_watch)
+        try:
+            os.remove(self.database)
+        except FileNotFoundError:
+            pass
+
+    def test_file_stream_duckdb_end_to_end(self):
+        app = buildflow.Flow()
+
+        @app.consumer(
+            source=FileChangeStream(file_path=self.dir_to_watch),
+            sink=AnalysisTable(table_name=self.table),
+            num_cpus=0.5,
+        )
+        def my_consumer(event: FileChangeEvent) -> Dict[str, str]:
+            return event.metadata
 
         run_coro = app.run(block=False)
 
         # wait for 20 seconds to let it spin up
         run_coro = self.run_for_time(run_coro, time=20)
 
-        response = requests.post(
-            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
-        )
-        response.raise_for_status()
-        self.assertEqual(response.json(), {"val": 2})
+        create_path = os.path.join(self.dir_to_watch, "file.txt")
+        with open(create_path, "w") as f:
+            f.write("hello")
 
+        run_coro = self.run_for_time(run_coro, time=20)
+
+        database = os.path.join(os.getcwd(), "buildflow_managed.duckdb")
+        conn = duckdb.connect(database=database, read_only=True)
+        got_data = conn.execute(f"SELECT count(*) FROM {self.table}").fetchone()
+
+        self.assertEqual(got_data[0], 1)
         self.get_async_result(app._drain())
 
-    def test_endpoint_end_to_end_class(self):
+    def test_file_stream_file_end_to_end(self):
+        output_dir = tempfile.mkdtemp()
+        try:
+            app = buildflow.Flow()
+
+            @app.consumer(
+                source=FileChangeStream(file_path=self.dir_to_watch),
+                sink=File(
+                    os.path.join(output_dir, "file.csv"), file_format=FileFormat.CSV
+                ),
+                num_cpus=0.5,
+            )
+            def my_consumer(event: FileChangeEvent) -> Dict[str, str]:
+                return {"content": event.blob.decode()}
+
+            run_coro = app.run(block=False)
+
+            # wait for 20 seconds to let it spin up
+            run_coro = self.run_for_time(run_coro, time=20)
+
+            create_path = os.path.join(self.dir_to_watch, "hello.txt")
+            with open(create_path, "w") as f:
+                f.write("hello")
+
+            create_path = os.path.join(self.dir_to_watch, "world.txt")
+            with open(create_path, "w") as f:
+                f.write("world")
+
+            run_coro = self.run_for_time(run_coro, time=20)
+
+            output_files = os.listdir(output_dir)
+            self.assertEqual(len(output_files), 1)
+            output_file = os.path.join(output_dir, output_files[0])
+
+            with open(output_file, "r") as f:
+                lines = f.readlines()
+                # NOTE: for some reason these come out of order sometimes.
+                # I think it's due to the local file system notifications
+                # being delivered out of order.
+                lines.sort()
+                self.assertEqual(len(lines), 3)
+                self.assertEqual(lines[0], '"content"\n')
+                self.assertEqual(lines[1], '"hello"\n')
+                self.assertEqual(lines[2], '"world"\n')
+
+            self.get_async_result(app._drain())
+        finally:
+            shutil.rmtree(output_dir)
+
+    def test_file_stream_duckdb_end_to_end_unattached(self):
         app = buildflow.Flow()
-        service = app.service(num_cpus=0.5)
 
-        @service.endpoint(route="/test", method="POST")
-        class Endpoint:
-            def setup(self):
-                self.val_to_add = 10
+        @buildflow.consumer(
+            source=FileChangeStream(file_path=self.dir_to_watch),
+            sink=AnalysisTable(table_name=self.table),
+            num_cpus=0.5,
+        )
+        def my_consumer(event: FileChangeEvent) -> Dict[str, str]:
+            return event.metadata
 
-            def get_val_to_add(self):
-                return self.val_to_add
-
-            def process(self, input: InputRequest) -> OutputResponse:
-                return OutputResponse(input.val + self.get_val_to_add())
+        app.add_consumer(my_consumer)
 
         run_coro = app.run(block=False)
 
         # wait for 20 seconds to let it spin up
         run_coro = self.run_for_time(run_coro, time=20)
 
-        response = requests.post(
-            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
-        )
-        response.raise_for_status()
-        self.assertEqual(response.json(), {"val": 11})
+        create_path = os.path.join(self.dir_to_watch, "file.txt")
+        with open(create_path, "w") as f:
+            f.write("hello")
 
-        self.get_async_result(app._drain())
-
-    def test_endpoint_end_to_end_detached(self):
-        app = buildflow.Flow()
-        service = buildflow.Service(num_cpus=0.5)
-        app.add_service(service)
-
-        @service.endpoint(route="/test", method="POST")
-        def my_endpoint(input: InputRequest) -> OutputResponse:
-            return OutputResponse(input.val + 1)
-
-        run_coro = app.run(block=False)
-
-        # wait for 20 seconds to let it spin up
         run_coro = self.run_for_time(run_coro, time=20)
 
-        response = requests.post(
-            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
-        )
-        response.raise_for_status()
-        self.assertEqual(response.json(), {"val": 2})
+        database = os.path.join(os.getcwd(), "buildflow_managed.duckdb")
+        conn = duckdb.connect(database=database, read_only=True)
+        got_data = conn.execute(f"SELECT count(*) FROM {self.table}").fetchone()
 
+        self.assertEqual(got_data[0], 1)
         self.get_async_result(app._drain())
 
-    def test_endpoint_dependencies(self):
+    def test_file_stream_duckdb_dependencies(self):
+        app = buildflow.Flow()
+
         @dependency(scope=Scope.NO_SCOPE)
         class NoScope:
             def __init__(self):
@@ -140,57 +184,43 @@ class EndpointLocalTest(unittest.TestCase):
                 self.val = 4
                 self.replica = replica
 
-        with tempfile.TemporaryDirectory() as output_dir:
-            sink = File(
-                os.path.join(output_dir, "file.csv"), file_format=FileFormat.CSV
-            )
-            SinkSideOutput = SinkDependencyBuilder(sink)
+        @app.consumer(
+            source=FileChangeStream(file_path=self.dir_to_watch),
+            sink=AnalysisTable(table_name=self.table),
+            num_cpus=0.5,
+        )
+        def my_consumer(
+            event: FileChangeEvent,
+            no: NoScope,
+            global_: GlobalScope,
+            replica: ReplicaScope,
+            process: ProcessScope,
+        ) -> Dict[str, int]:
+            if id(process.replica) != id(replica):
+                raise Exception("Replica scope not the same")
+            if id(replica.global_) != id(global_):
+                raise Exception("Global scope not the same")
+            if id(global_.no) == id(no):
+                raise Exception("No scope was the same")
+            return {"val": no.val + global_.val + replica.val + process.val}
 
-            app = buildflow.Flow()
-            service = app.service(num_cpus=0.5)
+        run_coro = app.run(block=False)
 
-            @service.endpoint(route="/test", method="POST")
-            async def my_endpoint(
-                input: InputRequest,
-                no: NoScope,
-                global_: GlobalScope,
-                replica: ReplicaScope,
-                process: ProcessScope,
-                sink: SinkSideOutput,
-            ) -> OutputResponse:
-                if id(process.replica) != id(replica):
-                    raise Exception("Replica scope not the same")
-                if id(replica.global_) != id(global_):
-                    raise Exception("Global scope not the same")
-                if id(global_.no) == id(no):
-                    raise Exception("No scope was the same")
-                to_write = input.val + no.val + global_.val + replica.val + process.val
-                to_return = OutputResponse(to_write)
-                await sink.push(to_return)
-                return OutputResponse(to_write)
+        # wait for 20 seconds to let it spin up
+        run_coro = self.run_for_time(run_coro, time=20)
 
-            run_coro = app.run(block=False)
+        create_path = os.path.join(self.dir_to_watch, "file.txt")
+        with open(create_path, "w") as f:
+            f.write("hello")
 
-            # wait for 20 seconds to let it spin up
-            run_coro = self.run_for_time(run_coro, time=20)
+        run_coro = self.run_for_time(run_coro, time=20)
 
-            response = requests.post(
-                "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
-            )
-            response.raise_for_status()
-            self.assertEqual(response.json(), {"val": 11})
+        database = os.path.join(os.getcwd(), "buildflow_managed.duckdb")
+        conn = duckdb.connect(database=database, read_only=True)
+        got_data = conn.execute(f"SELECT * FROM {self.table}").fetchone()
 
-            output_files = os.listdir(output_dir)
-            self.assertEqual(len(output_files), 1)
-            output_file = os.path.join(output_dir, output_files[0])
-
-            with open(output_file, "r") as f:
-                lines = f.readlines()
-                self.assertEqual(len(lines), 2)
-                self.assertEqual(lines[0], '"val"\n')
-                self.assertEqual(lines[1], "11\n")
-
-            self.get_async_result(app._drain())
+        self.assertEqual(got_data[0], 10)
+        self.get_async_result(app._drain())
 
 
 if __name__ == "__main__":
--- end_to_end_tests/endpoint_test.py
+++ end_to_end_tests/endpoint_test.py
@@ -45,6 +45,27 @@ class EndpointLocalTest(unittest.TestCase):
 
         return self.event_loop.run_until_complete(wait_wrapper())
 
+    def test_endpoint_end_to_end(self):
+        app = buildflow.Flow()
+        service = app.service(num_cpus=0.5)
+
+        @service.endpoint(route="/test", method="POST")
+        def my_endpoint(input: InputRequest) -> OutputResponse:
+            return OutputResponse(input.val + 1)
+
+        run_coro = app.run(block=False)
+
+        # wait for 20 seconds to let it spin up
+        run_coro = self.run_for_time(run_coro, time=20)
+
+        response = requests.post(
+            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
+        )
+        response.raise_for_status()
+        self.assertEqual(response.json(), {"val": 2})
+
+        self.get_async_result(app._drain())
+
     def test_endpoint_end_to_end_class(self):
         app = buildflow.Flow()
         service = app.service(num_cpus=0.5)
@@ -66,7 +87,7 @@ class EndpointLocalTest(unittest.TestCase):
         run_coro = self.run_for_time(run_coro, time=20)
 
         response = requests.post(
-            "http://127.0.0.1:8000/test", json={"val": 1}, timeout=10
+            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
         )
         response.raise_for_status()
         self.assertEqual(response.json(), {"val": 11})
@@ -88,7 +109,7 @@ class EndpointLocalTest(unittest.TestCase):
         run_coro = self.run_for_time(run_coro, time=20)
 
         response = requests.post(
-            "http://127.0.0.1:8000/test", json={"val": 1}, timeout=10
+            "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
         )
         response.raise_for_status()
         self.assertEqual(response.json(), {"val": 2})
@@ -154,7 +175,7 @@ class EndpointLocalTest(unittest.TestCase):
             run_coro = self.run_for_time(run_coro, time=20)
 
             response = requests.post(
-                "http://127.0.0.1:8000/test", json={"val": 1}, timeout=10
+                "http://0.0.0.0:8000/test", json={"val": 1}, timeout=10
             )
             response.raise_for_status()
             self.assertEqual(response.json(), {"val": 11})
